7a20ec2457b9029678be73ddb8ffb4a9a2851e89
RomanSeps <96355457+RomanSeps@users.noreply.github.com>
2024-12-12 12:37:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const xddd = document.getElementsByClassName("grid")
+const size = 100;
+
+for(let i = 0; i<size; i++){
+    let xeee = document.createElement("div")
+    xddd[0].appendChild(xeee)
+    xeee.classList.add("kosticky")
+}
+
