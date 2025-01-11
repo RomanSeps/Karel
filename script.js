14e2ab406cbe61da72c2bfa5067d65deade1d533
RomanSeps <96355457+RomanSeps@users.noreply.github.com>
2025-01-12 00:26:44 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const xddd = document.getElementsByClassName("grid")
-const size = 100;
-
-for(let i = 0; i<size; i++){
-    let xeee = document.createElement("div")
-    xddd[0].appendChild(xeee)
-    xeee.classList.add("kosticky")
-}
-
